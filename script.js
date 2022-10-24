a8a81588532a80231bdc25c5b7794516825898f3
Oguz <aka@pop-os.localdomain>
2022-10-24 17:37:15 +0300

    added searchbar functions. only working for city names and state names atm

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 'use strict';
-// Variables
+const searchBar = document.querySelector('.search');
+const suggestions = document.querySelector('.suggestions');
 const endpoint =
   'https://gist.githubusercontent.com/Miserlou/c5cd8364bf9b2420bb29/raw/2bf258763cdddd704f8ffd3ea9a3e81d25e2c6f6/cities.json';
 const cityInfo = [];
@@ -7,4 +8,34 @@ const cityInfo = [];
 fetch(endpoint)
   .then((response) => response.json())
   .then((data) => cityInfo.push(...data));
-console.log(cityInfo);
+
+function findMatches(wordToMatch, cities) {
+  return cities.filter((place) => {
+    const regex = new RegExp(wordToMatch, 'gi');
+    return place.city.match(regex) || place.state.match(regex);
+  });
+}
+
+function displayMatches() {
+  const matchArray = findMatches(this.value, cityInfo);
+  const html = matchArray
+    .map((place) => {
+      const regex = new RegExp(this.value, 'gi');
+      const cityName = place.city.replace(
+        regex,
+        `<span class='hl'>${this.value}</span>`
+      );
+      const stateName = place.state.replace(
+        regex,
+        `<span class='hl'>${this.value}</span>`
+      );
+      return ` 
+    <li>
+    <span class="name">${cityName}, ${stateName}</span>
+    </li>`;
+    })
+    .join('');
+  suggestions.innerHTML = html;
+}
+
+searchBar.addEventListener('keyup', displayMatches);
